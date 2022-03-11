e790de3e950c615f7bec46aebe1d5823b20d1307
Murilo Maia <murilomaia.bb@gmail.com>
2022-03-10 21:41:41 -0300

    refactor: get entities automatically

--- src/infra/typeorm/TypeormHelper.ts
+++ src/infra/typeorm/TypeormHelper.ts
@@ -3,7 +3,6 @@ import fs from 'fs';
 import path from 'path';
 import { Connection, createConnection } from 'typeorm';
 import env from '@/main/config/env';
-import { entities } from './entities';
 
 export class TypeormHelper {
   private connection?: Connection;
@@ -19,6 +18,7 @@ export class TypeormHelper {
 
   async connect(): Promise<void> {
     const migrations = await this.getMigrations();
+    const entities = await this.getEntities();
     this.connection = await createConnection({
       ...env.db,
       migrations,
@@ -42,5 +42,23 @@ export class TypeormHelper {
         });
     }
     return migrations;
+  }
+  private async getEntities() {
+    const entitiesDir = path.join(__dirname, 'entities');
+    const entities: any[] = [];
+    const dirExists = fs.existsSync(entitiesDir);
+    if (dirExists) {
+      const files = await readdir(entitiesDir, { encoding: 'utf-8' });
+      files
+        .filter(file => file.includes('DB.'))
+        .forEach(async file => {
+          console;
+          const imports = await import(`${entitiesDir}/${file}`);
+          const [className] = file.split('.');
+          entities.push(imports[className]);
+        });
+    }
+
+    return entities;
   }
 }
--- src/infra/typeorm/entities/index.ts
+++ src/infra/typeorm/entities/index.ts
@@ -1,7 +1,2 @@
-import { CustomerDB } from './CustomerDB';
-import { PlanDB } from './PlanDB';
-
 export * from './PlanDB';
 export * from './CustomerDB';
-
-export const entities = [PlanDB, CustomerDB];
