3d0be65627e71054ba92a08fa46d18360250ecbf
Murilo Maia <murilomaia.bb@gmail.com>
2022-03-11 01:33:38 -0300

    feat: implement HashProvider

--- src/main/factories/controllers/makeCreateCustomersController.ts
+++ src/main/factories/controllers/makeCreateCustomersController.ts
@@ -1,10 +1,12 @@
 import { CreateCustomersController } from '@/application/controllers/customers/CreateCustomersController';
 import { CreateCustomer } from '@/domain/features/CreateCustomer';
+import { HashProvider } from '@/infra/providers';
 import { repositories } from '../repositories';
 
 export function makeCreateCustomersController() {
   const customersRepository = repositories.ICustomersRepository;
   const plansRepository = repositories.IPlansRepository;
-  const service = new CreateCustomer(customersRepository, plansRepository, { hash: async tohash => tohash });
+  const hashProvider = new HashProvider(12);
+  const service = new CreateCustomer(customersRepository, plansRepository, hashProvider);
   return new CreateCustomersController(service);
 }
